c0e87dff036740263d82bc1883f038e8ff7adcda
gabriel <goicoecheagabriel@gmail.com>
2022-02-05 13:34:32 +0100

    Ocultamos el filtro de la tienda de Ecwid para que no se muestre abierto por defecto.

--- src/app/store/pages/ecwid/ecwid.component.ts
+++ src/app/store/pages/ecwid/ecwid.component.ts
@@ -94,6 +94,8 @@ export class EcwidComponent implements OnInit, OnChanges, OnDestroy {
     xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-54091005");
 
 
+
+
   }
 
   ngDoCheck(){
