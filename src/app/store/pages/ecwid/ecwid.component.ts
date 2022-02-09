fc9f1b479d87bcf671d002232cd95aee9b6e743d
gabriel <goicoecheagabriel@gmail.com>
2022-02-09 13:23:09 +0100

    limpieza en componente ecwid.component.ts

--- src/app/store/pages/ecwid/ecwid.component.ts
+++ src/app/store/pages/ecwid/ecwid.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit, Renderer2, OnChanges, NgZone, OnDestroy } from '@angular/core';
 
 import { environment } from 'src/environments/environment';
-import { EcwidService } from '../../services/ecwid.service';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { of, Subscription } from 'rxjs';
-import { PlatformLocation } from '@angular/common';
-import { CarritoService } from '../../services/carrito.service';
 import { Carrito } from '../../interfaces/carrito.interface';
 
 declare var window: any;
@@ -18,51 +13,11 @@ declare var xProductBrowser:any;
   templateUrl: './ecwid.component.html',
   styleUrls: ['./ecwid.component.css']
 })
-export class EcwidComponent implements OnInit, OnChanges, OnDestroy {
+export class EcwidComponent implements OnInit {
 
   storeId: number = environment.storeId;
-  navigationSubscription: Subscription;
 
-
-
-  constructor( public ecwidService: EcwidService,
-              //  private _renderer2:Renderer2,
-              //  private route: ActivatedRoute,
-               private _router: Router,
-               private platformLocation: PlatformLocation,
-               private ngZone: NgZone ) {
-    this.platformLocation.onPopState( (a) => {
-      if( this.platformLocation.pathname.startsWith('/store') ) {
-        this.ngZone.run( () => {
-          console.warn("Reloading component ECWID",a)
-
-
-        } )
-      }
-    } );
-
-    this.platformLocation.onHashChange((a)=>{
-        this.ngZone.run( () => {
-          console.warn("Reloading component ECWID:::hash",a)
-
-
-         } )
-    } )
-
-    //Suscribirse a los eventos del enrutador
-    this.navigationSubscription = this._router.events.subscribe( ( e:any ) =>{
-      // Si se trata de un evento de NavigationEnd, reinicie el componente
-      if( e instanceof NavigationEnd ) {
-        this.initialiseInvites()
-      }
-    } )
-
-
-   }
-
-
-
-   initialiseInvites(){
+  constructor() {
   }
 
   ngOnInit(): void {
@@ -71,42 +26,16 @@ export class EcwidComponent implements OnInit, OnChanges, OnDestroy {
     var callback = function(cart:Carrito){
       window.localStorage.setItem('carrito-service', JSON.stringify( cart ));
     }
+
     Ecwid.OnCartChanged.add(callback);
 
-    // recreamos la tienda de ecwid cada vez que se activa el componente
-    console.warn(":::ngOnInit::ecwid.component");
-    window.ecwid_script_defer = true;
-    window.ecwid_dynamic_widgets = true;
-
-    console.log("TYPEOFF ECWID", typeof Ecwid)
-    if (typeof Ecwid !== 'undefined') {
-    // if (typeof Ecwid != 'undefined' && typeof Ecwid.destroy == 'function'){
-      console.log("pasamos por destroy");
-      Ecwid.destroy();
-
-    }
-    window._xnext_initialization_scripts = [{
-      widgetType: 'ProductBrowser',
-      id: 'my-store-54091005',
-      arg: ["id=productBrowser"]
-    }];
-
+    // Ecwid.init();
     xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-54091005");
 
-
-
-
-  }
-
-  ngDoCheck(){
-  }
-
-  ngOnChanges(){
   }
 
   ngOnDestroy(): void {
-    this.navigationSubscription.unsubscribe()
-    Ecwid.destroy();
+    // Ecwid.destroy();
   }
 
 
