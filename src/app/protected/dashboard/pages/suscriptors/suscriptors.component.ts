f428b0f2a8e0c4334560b18306ff52a000517dc0
gabriel <goicoecheagabriel@gmail.com>
2022-02-14 12:47:51 +0100

    Implementamos una tabla de suscriptores en el backend y creamos la exportacion de los suscriptores en un excel en caso de que se quiera descargar

--- src/app/protected/dashboard/pages/suscriptors/suscriptors.component.ts
+++ src/app/protected/dashboard/pages/suscriptors/suscriptors.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { SuscriptorsService } from '../../services/suscriptors.service';
 import { Suscriptor } from '../../interfaces/suscriptores.interface'
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-suscriptors',
@@ -31,6 +33,23 @@ selectedProducts!: Suscriptor[];
 
   }
 
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.selectedProducts || this.suscriptores);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "suscriptores");
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+}
 
 
 }
